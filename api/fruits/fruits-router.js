742cbce68e546af6ab703ae6446efeba583dab7e
Kat <kyevsukov@gmail.com>
2021-11-02 16:24:30 -0400

    updated post function

--- api/fruits/fruits-router.js
+++ api/fruits/fruits-router.js
@@ -35,14 +35,30 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// router.post('/', (req, res) => {  <===TRASH!!!
+//   const fruitData = req.body;
+//   db('fruits').insert(fruitData)
+//     .then(ids => {
+//       db('fruits').where({ id: ids[0] })
+//         .then(newFruitEntry => {
+//           res.status(201).json(newFruitEntry);
+//         });
+//     })
+//     .catch(err => {
+//       console.log('POST error', err);
+//       res.status(500).json({ message: "Failed to store data" });
+//     });
+// });
+
+//BETTER 
 router.post('/', (req, res) => {
   const fruitData = req.body;
   db('fruits').insert(fruitData)
     .then(ids => {
-      db('fruits').where({ id: ids[0] })
-        .then(newFruitEntry => {
-          res.status(201).json(newFruitEntry);
-        });
+      return db('fruits').where({ id: ids[0] })
+    })
+    .then(newFruitEntry => {
+      res.status(201).json(newFruitEntry);
     })
     .catch(err => {
       console.log('POST error', err);
